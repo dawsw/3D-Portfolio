5e399f8f0a784e5a04b0f73f7737fb63fe944859
dweila1 <dweila1@wgu.edu>
2025-12-09 21:00:44 -0600

    changed base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-<<<<<<< HEAD
   base: '/3d/' 
-=======
-  base: '/', 
 
->>>>>>> 3a0f0850006523c6e4018ab1bc575ce172c8a446
 });
